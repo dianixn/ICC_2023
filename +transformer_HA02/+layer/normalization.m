f3005c70f66e1d7b0a4a3c83cd8daec7dd4b702f
Dianxin Luan <49792786+dianixn@users.noreply.github.com>
2023-08-06 00:13:15 +0100

    Update normalization.m

--- +transformer_HA02/+layer/normalization.m
+++ +transformer_HA02/+layer/normalization.m
@@ -1,21 +1,4 @@
 function Z = normalization(X, g, b)
-% normalization   Layer Normalization
-%
-%   Z = normalization(X, g, b) applies layer normalization to the input X.
-%   Layer normzalization is described in [1].
-%
-%   Inputs:
-%       X - A numFeatures-by-numInputSubwords-by-numObs input array.
-%       g - A numFeatures-by-1 weight vector.
-%       b - A numFeatures-by-1 bias vector.
-%
-%   Outputs:
-%       Z - A numFeatures-by-numInputSubwords-by-numObs output array.
-%
-%   References:
-%
-%   [1] Jimmy Lei Ba, Jamie Ryan Kiros, Geoffrey E. Hinton, "Layer
-%       Normalization", https://arxiv.org/abs/1607.06450
 
 normalizationDimension = 1;
 
