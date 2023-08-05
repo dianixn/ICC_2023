809f3e49ab70bdc72e7ed6a34a85dbfaed6544a2
Dianxin Luan <49792786+dianixn@users.noreply.github.com>
2023-08-06 00:05:45 +0100

    Update parameters.m

--- +Parameter/parameters.m
+++ +Parameter/parameters.m
@@ -23,12 +23,6 @@ Carrier_Frequency = 2.1e9;
 Max_Mobile_Speed = 50; % km/h
 
 SampleRate = Num_of_subcarriers * Frequency_Spacing;
-%PathDelays = [0 30 70 90 110 190 410] * 1e-9; % EPA
-%AveragePathGains = [0 -1 -2 -3 -8 -17.2 -20.8]; % EPA
-%PathDelays = [0 30 150 310 370 710 1090 1730 2510] * 1e-9; % EVA
-%AveragePathGains = [0 -1.5 -1.4 -3.6 -0.6 -9.1 -7 -12 -16.9]; % EVA
-%PathDelays = [0 50 120 200 230 500 1600 2300 5000] * 1e-9; % ETU
-%AveragePathGains = [-1.0 -1.0 -1.0 0.0 0.0 0.0 -3.0 -5.0 -7.0]; % ETU
 
 %PathDelays = [0 50 100 200 400] * 1e-9; % DC1
 %AveragePathGains = [0.0 -2.0 -4.0 -8.0 -16.0]; % DC1
@@ -42,21 +36,9 @@ SampleRate = Num_of_subcarriers * Frequency_Spacing;
 PathDelays = 0; 
 AveragePathGains = 0.0; 
 
-%PathDelays = [0 30 200 300 500 1500 2500 5000 7000 9000] * 1e-9 + (7/SampleRate)*1e9; 
-%AveragePathGains = [0.0 0 0 0.0 0.0 0.0 -1.0 -1.0 -2.0 -4.0]; 
-
-%PathDelays = [0:500:6500] * 1e-9; % CE channel 
-%AveragePathGains = zeros(1, size(PathDelays, 2)); % CE channel 
-
 %PathDelays = [50 5000] * 1e-9; % Two-path channel
 %AveragePathGains = [-3.0 -3.0]; % Two-path channel
 
-%PathDelays = [0 30 200 300 500 1500 2500 5000 7000 9000] * 1e-9; % T
-%AveragePathGains = [0.0 0 0 0.0 0.0 0.0 -1.0 -1.0 -2.0 -4.0]; % T
-
-%PathDelays = [0:1000:7000] * 1e-9; 
-%AveragePathGains = 0 * ones(1, size(PathDelays, 2)); 
-
 MaxDopplerShift = floor((Carrier_Frequency * Max_Mobile_Speed) / (3e8 * 3.6));
 
-DelayProfile = 'Custom'; % 'EPA' 'EVA' 'ETU' 'Custom' 'CDL-A' 'CDL-B' 'CDL-C' 
+DelayProfile = 'ETU'; % 'EPA' 'EVA' 'ETU' 'Custom' 'CDL-A' 'CDL-B' 'CDL-C' 
