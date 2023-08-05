aa0f9b400ee8eeea4d40b6b467f077df3afa9004
Dianxin Luan <49792786+dianixn@users.noreply.github.com>
2023-08-06 00:10:07 +0100

    Update FC1.m

--- +transformer_HA02/+layer/FC1.m
+++ +transformer_HA02/+layer/FC1.m
@@ -1,17 +1,5 @@
 function Z = FC1(X, W, b)
 
-% convolution1d   A fully connected layer
-%
-%   Z = convolution1d(X, W, b) applies a fully connected layer.
-%
-%   Inputs:
-%       X   - A numInputFeatures-by-numInputSubwords array.
-%       W   - A numOutputFeatures-by-numInputFeatures weight matrix.
-%       b   - A numOutputFeatures-by-1 bias vector.
-%
-%   Output:
-%       Z   - A numOutputFeatures-by-numInputSubwords array.
-
 Z = dlmtimes(W,X) + b;
 
 end
